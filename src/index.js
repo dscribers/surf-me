ee6118bb37d18e84d56489e03b5e1c6aea8a3a8c
Ezra Obiwale <contact@ezraobiwale.com>
2020-04-19 15:31:36 +0100

    Updates AutoSurf
    - Updates flow

--- src/index.js
+++ src/index.js
@@ -1,79 +1,23 @@
-import AutoSurf from '@dscribers/autosurf'
-
 // in an iframe
 if (window.parent !== window) {
-  const surf = function() {
-    window.addEventListener('message', receivedCommand, false)
+  const $surfer = new require('@dscribers/autosurf')()
 
-    const Events = {
-      listenToEvents() {
-        this.scheduler = AutoSurf.on('*', function(event) {
-          window.parent.postMessage(event, '*')
-        })
-      },
-      pause() {
-        if (this.scheduler) {
-          this.scheduler.pause()
-        }
-      },
-      restart() {
-        if (this.scheduler) {
-          this.scheduler.restart()
-        }
-      },
-      resume() {
-        if (this.scheduler) {
-          this.scheduler.resume()
-        }
-      },
-      start(feature) {
-        this.listenToEvents()
-        this.scheduler
-          .parseFeature(feature)
-          .clearBackup()
-          .start({
-            debug: true,
-            delayBetweenSchedules: 3000
-          })
+  $surfer
+    .on('*', function (event) {
+      window.parent.postMessage(event, '*')
+    })
+    .ready((isNotNew) => {
+      if (!isNotNew) {
+        // @todo: change * to testsuite live site origin
+        window.parent.postMessage({ name: 'ready' }, '*')
       }
+    })
+
+  function receivedCommand({ data = {} }) {
+    if ($surfer.hasOwnProperty(data.name)) {
+      $surfer[data.name](data.detail)
     }
-
-    function receivedCommand({ data = {} }) {
-      if (Events.hasOwnProperty(data.name)) {
-        Events[data.name](data.detail)
-      }
-    }
-
-    AutoSurf.ready(
-      isNotNew => {
-        if (isNotNew) {
-          Events.listenToEvents()
-        }
-      },
-      isNotNew => {
-        if (!isNotNew) {
-          // @todo: change * to testsuite live site origin
-          window.parent.postMessage({ name: 'ready' }, '*')
-        }
-      }
-    )
   }
 
-  const oldLoadFunc = window.onload
-  window.onload = function() {
-    if (typeof oldLoadFunc === 'function') {
-      oldLoadFunc()
-    }
-
-    surf()
-  }
-
-  const oldBeforeUnloadFunc = window.onbeforeunload
-  window.onbeforeunload = function() {
-    if (typeof oldBeforeUnloadFunc === 'function') {
-      oldBeforeUnloadFunc()
-    }
-
-    AutoSurf.backup()
-  }
+  window.addEventListener('message', receivedCommand, false)
 }
